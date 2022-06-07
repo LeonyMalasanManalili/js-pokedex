f7ce6974bf3003e9027a638fc7ed253f8ded5e0d
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-07 02:42:28 +0200

    First commit - needs to be studied more.

---
+++ main.js
@@ -0,0 +1,37 @@
+//Creazione Variabile Pokedex --> seleziono il contenitore dove andro a lavorare
+let pokedex = document.querySelector('.contenitorePokemon');
+
+// faccio un ciclo dove utilizzero il "fetch()" + incremento e in caso positivo ovvero "then()" restituiscimi un valore result.json() ancora se positivo
+// allora creo un parametro con un oggetto dove prendero dall api immagine nome id e tipo di pokemon
+for(i = 1; i <= 150; i++) {
+    fetch('https://pokeapi.co/api/v2/pokemon/' +i)
+    .then(result => result.json())
+    .then(pokemon => inputPokemon(
+        pokemon.sprites.front_default,
+        pokemon.name,
+        pokemon.id,
+        pokemon.types[0].type.name
+    ));
+}
+
+// Creazione dell oggetto  utilizzato dentro il ciclo che stampera le card
+// In questo span stampo la stringa "# + id,trasformato in stringa e con metodo padStart() vuole come primo argomento da dove deve iniziare a mettere
+// la stringa e nel secondo valore cosa mettere prima di quell'inizio. 
+function inputPokemon (showpokemon, name, id, type) {
+    pokedex.innerHTML += `        
+    <div id="dv_${id}" class="schedaPokemon ${type}">
+    <div class="showPokemon">
+        <img src="${showpokemon}">
+    </div>
+    <div class="pokeInformations">
+        <span class="whichPokemon">#${id.toString().padStart(3, "0")}</span>
+        <p>${name}</p>
+        <p>Type: ${type}</p>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+
+
